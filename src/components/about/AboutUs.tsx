81e237c1d3c92ae605549abd0e391f3c78c2c738
Angel Zabaleta Torrez <angelzabaleta08@hotmail.com>
2024-04-24 17:19:27 -0500

    Corrección altura about

--- src/components/about/AboutUs.tsx
+++ src/components/about/AboutUs.tsx
@@ -17,14 +17,14 @@ export default function AboutUs() {
 
         )}
       </div>
-      <div className='p-20'>
+      <div className='px-[10vh] pt-[10vh]'>
         <h1 className='flex justify-center  headerTitleText'>Acerca de TechPathFinder</h1>
         <div className='aboutDescriptionText'>
-          <p className='py-10'>TechPathFinder es una plataforma web diseñada para ofrecer orientación estratégica a estudiantes y profesionales interesados en el campo de las tecnologías de la información. En un mundo donde la tecnología avanza rápidamente, puede ser abrumador mantenerse al día con las últimas tendencias y habilidades demandadas por el mercado laboral.</p>
+          <p className='py-[6vh]'>TechPathFinder es una plataforma web diseñada para ofrecer orientación estratégica a estudiantes y profesionales interesados en el campo de las tecnologías de la información. En un mundo donde la tecnología avanza rápidamente, puede ser abrumador mantenerse al día con las últimas tendencias y habilidades demandadas por el mercado laboral.</p>
 
-          <p className='py-10'>Nuestra misión en TechPathFinder es ayudarte a navegar por este complejo panorama tecnológico, brindándote información actualizada sobre las habilidades más demandadas, las tendencias del mercado laboral y las oportunidades de carrera en el campo de la tecnología de la información.</p>
+          <p className='py-[3vh]'>Nuestra misión en TechPathFinder es ayudarte a navegar por este complejo panorama tecnológico, brindándote información actualizada sobre las habilidades más demandadas, las tendencias del mercado laboral y las oportunidades de carrera en el campo de la tecnología de la información.</p>
 
-          <p className='py-10'>Con el respaldo de datos sólidos y análisis detallados, nuestra plataforma te ayudará a tomar decisiones informadas sobre tu desarrollo profesional, identificar las áreas de especialización más relevantes y trazar un camino claro hacia el éxito en el emocionante mundo de la tecnología.</p>
+          <p className='py-[3vh]'>Con el respaldo de datos sólidos y análisis detallados, nuestra plataforma te ayudará a tomar decisiones informadas sobre tu desarrollo profesional, identificar las áreas de especialización más relevantes y trazar un camino claro hacia el éxito en el emocionante mundo de la tecnología.</p>
 
           <p>Únete a nosotros en TechPathFinder y comienza a explorar tu futuro en la tecnología de la información hoy mismo.</p>
         </div>
